db2f2a2b57a28e8a770f3a4a974413e8147f4489
OliverTryding <82950462+OliverTryding@users.noreply.github.com>
2022-09-21 14:36:28 +0200

    Version 1.0
    Finished all tasks except for testing different n.

--- Projects/Project0/src/fraction_summing/fraction_toolbox.cpp
+++ Projects/Project0/src/fraction_summing/fraction_toolbox.cpp
@@ -21,44 +21,100 @@ void print_fraction_array(fraction frac_array[], int n)
 fraction square_fraction(fraction frac)
 {
     //TODO: implement function 2
+        fraction square;
+        square.num = frac.num * frac.num;
+        square.denom = frac.denom * frac.denom;
+        return square;
 }
 
 //TODO: implement function 3
+void square_fraction_inplace(fraction & frac) 
+{
+        frac.num = frac.num * frac.num;
+        frac.denom = frac.denom * frac.denom;
+}
 
 
 double fraction2double(fraction frac)
 {
     //TODO: implement function 4
+        double double_num = (double) frac.num;
+        double double_denom = (double) frac.denom;
+        return double_num / double_denom;
 }
 
-int gcd(int a, int b)
+int gcd(int a, int b) 
 {
     //TODO: implement function 5
+        if(b == 0) { 
+                return a;
+        }
+        else { 
+                return gcd(b,a % b);
+        }
 }
 
 //TODO: implement function 6
+int gcd(fraction frac) 
+{ 
+    int a = frac.num;
+    int b = frac.denom;
+    while(b != 0) { 
+            int t = b;
+            b = a % b;
+            a = t;
+    }
+    return a;
+}
 
 
 void reduce_fraction_inplace(fraction & frac)
 {
     //TODO: implement function 7
+    int greatest_common_divisor = gcd(frac.num,frac.denom);
+
+    frac.num = frac.num / greatest_common_divisor;
+    frac.denom = frac.denom / greatest_common_divisor;
 
     //TODO: add short comment to explain which of the gcd() functions your code is calling
+    //This function uses the iterative version of gcd from line 56. 
+    //This is because this version is loaded after the one on line 45 and therefore overloads gcd from line 45.
 }
 
-fraction add_fractions(fraction frac1, fraction frac2)
+fraction add_fractions(fraction frac1, fraction frac2) 
 {
     //TODO: implement function 8
+    fraction frac;
+    frac.num = frac1.num * frac2.denom + frac2.num * frac1.denom;
+    frac.denom = frac1.denom * frac2.denom;
+    reduce_fraction_inplace(frac);
+
+    return frac;
 }
 
-double sum_fraction_array_approx(fraction frac_array[], int n)
+double sum_fraction_array_approx(fraction frac_array[], int n) 
 {
     //TODO: implement function 9
-    
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+            sum += fraction2double(frac_array[i]);
+    }
+    return sum;
     //TODO: add short comment to explain why this function is approximate
+    //The function is an approximation because some fraction can not be made 
+    //into exact floating point numbers.
 }
 
 //TODO: implement function 10
+fraction sum_fraction_array(fraction frac_array[], int n) 
+{
+    fraction sum = frac_array[0];
+    for (int i = 1; i < n; i++) {
+            sum = add_fractions(sum,frac_array[i]);
+    }
+    return sum;
+}
+
 
 
 void fill_fraction_array(fraction frac_array[], int n)
--- Projects/Project0/src/fraction_summing/main.cpp
+++ Projects/Project0/src/fraction_summing/main.cpp
@@ -27,16 +27,53 @@ static void readcmdline(fraction & frac, int argc, char* argv[])
 static void test23467(int argc, char* argv[])
 {
     //TODO: implement function
+    fraction frac;
+
+    readcmdline(frac, argc, argv);
+
+    printf("The original fraction: ");
+    print_fraction(frac);
+
+    printf("Square of the fraction using function 2: ");
+    print_fraction(square_fraction(frac));
+
+    printf("Square of the fraction using function 3: ");
+    square_fraction_inplace(frac);
+    print_fraction(frac);
+
+    printf("Fraction to double using function 4: %lf\n", fraction2double(frac));
+
+    printf("GCD using function 6: %d\n", gcd(frac));
+
+    printf("Reduction of the fraction using function 7: ");
+    reduce_fraction_inplace(frac);
+    print_fraction(frac);
 }
 
 static void test5()
 {
     //TODO: implement function
+    int a;
+    int b;
+    printf("Give an integer: ");
+    std::cin >> a;
+    printf("Give another integer: ");
+    std::cin >> b;
+
+    printf("GCD using function 5: %d",gcd(a,b));
 }
 
 static void test_array_functions(int n)
 {
     //TODO: implement function
+    fraction* frac_array = (fraction *) malloc(n);
+
+    fill_fraction_array(frac_array,n);
+
+    printf("Sum of the fractions using function 10: ");
+    print_fraction(sum_fraction_array(frac_array,n));
+
+    printf("Sum of the fractions using function 9: %lf\n",sum_fraction_array_approx(frac_array,n));
 
     //TODO: find n for which sum function breaks. Explain what is happening.
 }
@@ -50,11 +87,11 @@ static void test_toolbox(int argc, char* argv[])
     test5();
 
     cout << "\n==========  test_array_functions  ========= " << endl;
-    int n = 5;
+    int n = 1000;
     test_array_functions(n);
 }
 
 int main(int argc, char* argv[])
 {
-    
+    test_toolbox(argc,argv);
 }
